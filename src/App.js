08074e6370ca8ed28324cb9bcc453d6aac7a79e7
Rob Thijssen <rthijssen@gmail.com>
2020-03-09 22:27:40 +0200

    attempt png conversion

--- src/App.js
+++ src/App.js
@@ -16,10 +16,34 @@ class App extends React.Component {
       text: (this.qs.v === undefined || this.qs.v === null) ? 'https://threetinydots.github.io/qr' : decodeURI(this.qs.v),
       level: 'Q',
       background: (this.qs.b === undefined || this.qs.b === null) ? '#ffffff' : this.qs.b,
-      foreground: (this.qs.f === undefined || this.qs.f === null) ? '#000000' : this.qs.f
+      foreground: (this.qs.f === undefined || this.qs.f === null) ? '#000000' : this.qs.f,
+      qrcodeAsBase64: null
     };
-    //this.displayEvents = this.displayEvents.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick () {
+    let canvas = document.createElement('canvas');
+    let context = canvas.getContext('2d');
+    //let svg = document.getElementById('qrcode');//.querySelector('svg');
+    let image = new Image(this.state.size, this.state.size);
+    image.src = this.state.qrcodeAsBase64;
+    context.drawImage(image, 0, 0);
+    let uri = canvas
+      .toDataURL('image/png')
+      .replace('image/png', 'image/octet-stream');
+    //window.location = uri;
+    let a = document.createElement('a');
+    a.setAttribute('download', 'qrcode.png');
+    a.setAttribute('href', uri);
+    a.setAttribute('target', '_blank');
+    a.dispatchEvent(new MouseEvent('click', {
+      view: window,
+      bubbles: false,
+      cancelable: true
+    }));
+  }
+
   render() {
     return (
       <Container>
@@ -33,7 +57,7 @@ class App extends React.Component {
               placeholder="https://threetinydots.github.io/qr" />
           </Form.Group>
           <Form.Group>
-            <Form.Label>size</Form.Label>
+            <Form.Label>size {this.state.size}</Form.Label>
             <Slider 
               value={this.state.size}
               min={100}
@@ -57,7 +81,12 @@ class App extends React.Component {
             fgColor={this.state.foreground}
             level={this.state.level}
             style={{ width: this.state.size }}
-            value={this.state.text} />
+            value={this.state.text}
+            id="qrcode"
+            onLoad={(event) => this.setState({ qrcodeAsBase64: 'data:image/svg+xml;base64,' + btoa(new XMLSerializer().serializeToString(event.target.getSVGDocument())) })} />
+          <div>
+            <button onClick={this.handleClick}>Click to download</button>
+          </div>
         </Form>
       </Container>
     );
